d4f7b711a76369eb291941db5e9d84fad418c51e
VLitvinenko <vlitvinenko@roox.ru>
2016-12-14 22:09:40 +0300

    enhance tests

--- pattern-matcher/src/test/java/lva/patternmatcher/SuffixTrieTest.java
+++ pattern-matcher/src/test/java/lva/patternmatcher/SuffixTrieTest.java
@@ -46,21 +46,27 @@ public class SuffixTrieTest {
     public void should_find_all_substrings_non_overlapped() {
         SuffixTrie<String> ast = new SuffixTrie<>(Collections.singletonList("abc"));
 
-        assertTrue(ast.query("abc").getResultSet().size() > 0);
-        assertTrue(ast.query("ab").getResultSet().size() > 0);
-        assertTrue(ast.query("bc").getResultSet().size() > 0);
-        assertTrue(ast.query("a").getResultSet().size() > 0);
-        assertTrue(ast.query("b").getResultSet().size() > 0);
-        assertTrue(ast.query("c").getResultSet().size() > 0);
+        assertEquals(0, getMatchingIndex(ast.query("abc"), "abc", 0, 3));
+        assertEquals(0, getMatchingIndex(ast.query("ab"), "abc", 0, 2));
+        assertEquals(0, getMatchingIndex(ast.query("bc"), "abc", 1, 3));
+        assertEquals(0, getMatchingIndex(ast.query("a"), "abc", 0, 1));
+        assertEquals(0, getMatchingIndex(ast.query("b"), "abc", 1, 2));
+        assertEquals(0, getMatchingIndex(ast.query("c"), "abc", 2, 3));
+
     }
 
     @Test
     public void should_find_all_substrings_overlapped() {
         SuffixTrie<String> ast = new SuffixTrie<>(Collections.singletonList("aaa"));
+        // TODO: reimplement
+        assertEquals(0, getMatchingIndex(ast.query("aaa"), "aaa", 0, 3));
 
-        assertTrue(ast.query("aaa").getResultSet().size() > 0);
-        assertTrue(ast.query("aa").getResultSet().size() > 0);
-        assertTrue(ast.query("a").getResultSet().size() > 0);
+        assertEquals(0, getMatchingIndex(ast.query("aa"), "aaa", 0, 2));
+        assertEquals(1, getMatchingIndex(ast.query("aa"), "aaa", 1, 3));
+
+        assertEquals(0, getMatchingIndex(ast.query("a"), "aaa", 0, 1));
+        assertEquals(1, getMatchingIndex(ast.query("a"), "aaa", 1, 2));
+        assertEquals(2, getMatchingIndex(ast.query("a"), "aaa", 2, 3));
     }
 
     @Test
@@ -70,7 +76,7 @@ public class SuffixTrieTest {
     }
 
     @Test
-    public void should_add_matchings_sorted_order() {
+    public void should_add_matchings_in_sorted_order() {
         SuffixTrie<String> ast = new SuffixTrie<>(Collections.singletonList("aaa"));
         MatchingResultSet<String> res = ast.query("a");
 
